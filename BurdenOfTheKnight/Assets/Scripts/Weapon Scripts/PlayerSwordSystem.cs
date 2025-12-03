0b3c2b71bf9a7bd1b1ee14014b837af1648d7ca6
Shehryyy <2274835@edu.vaniercollege.qc.ca>
2025-12-02 21:10:30 -0500

    yurrrrrrrrrrrrrrr

--- BurdenOfTheKnight/Assets/Scripts/Weapon Scripts/PlayerSwordSystem.cs
+++ BurdenOfTheKnight/Assets/Scripts/Weapon Scripts/PlayerSwordSystem.cs
@@ -198,6 +198,7 @@ public class PlayerSwordSystem : MonoBehaviour
     {
         (string direction, float strength) swipeData = cursorPath.GetSwipeData();
 
+
         // Attempt to compensate strength
         if (swipeData.direction == "DOWN" || swipeData.direction == "UP")
         {
@@ -315,10 +316,5 @@ public class PlayerSwordSystem : MonoBehaviour
 
         // Return nearest to center
         return nextTarget.transform;
-    }
-
-    public void OnDisable()
-    {
-        exitCombatMode();
     }
 }
