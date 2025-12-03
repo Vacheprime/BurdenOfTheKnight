a266cc908be0361373d19e922c9afcef8917ceae
lydia230 <lydia.ay4.la@gmail.com>
2025-12-03 02:27:18 -0500

    Added Health logic

--- BurdenOfTheKnight/Assets/Scripts/Player Scripts/PlayerManager.cs
+++ BurdenOfTheKnight/Assets/Scripts/Player Scripts/PlayerManager.cs
@@ -23,6 +23,14 @@ public class PlayerManager : MonoBehaviour
 
     private bool isDead = false;
 
+    // ======TEST============
+    public RectTransform healthFill;
+    public RectTransform spellsFill;
+    public float currentHealthFill = 0f;
+    public float currentSpellsFill = 300f;
+    public float minFill = 300f;
+    //=======================
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -39,19 +47,26 @@ public class PlayerManager : MonoBehaviour
     {
         magicPoints = 100;
         SetMagicPoints(magicPoints);
-
         currentHealth = maxHealth;
-
-        if (healthSlider != null)
-        {
-            healthSlider.maxValue = maxHealth;
-            healthSlider.value = maxHealth;
-        }
+        SetMagicSpell(0, -300);
     }
 
     void Update()
     {
+        if (currentHealthFill >= 300f)
+        {
+            currentHealthFill = 0f;
+            ReduceHealthTest(0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ReduceHealthTest(0, 45);
+        }
+
+    
         CalculateMagicPoints();
+        AugmentMagicPoints();
     }
 
     public float GetMagicPoints() => magicPoints;
@@ -113,5 +128,44 @@ public class PlayerManager : MonoBehaviour
     private void Die()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ReduceHealthTest(float left, float right)
+    {
+        currentHealthFill += right;
+
+        Vector2 min = healthFill.offsetMin;
+        Vector2 max = healthFill.offsetMax;
+
+        healthFill.offsetMin = new Vector2(left, min.y);
+        healthFill.offsetMax = new Vector2(-currentHealthFill, max.y);
+    }
+
+    public void SetMagicSpell(float left, float right)
+    {
+        
+        Vector2 min = spellsFill.offsetMin;
+        Vector2 max = spellsFill.offsetMax;
+
+        spellsFill.offsetMin = new Vector2(left, min.y);
+        spellsFill.offsetMax = new Vector2(right, max.y);
+    }
+
+    public void AugmentMagicPoints()
+    {
+        Debug.Log(currentSpellsFill);
+        float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensitivity;
+
+        if (loudness < threshold) loudness = 0;
+        else if (loudness > 20) loudness = 20;
+
+        currentSpellsFill -= loudness / 1500f;
+        // if (currentSpellsFill < 0) currentSpellsFill = 0;
+
+        Vector2 min = spellsFill.offsetMin;
+        Vector2 max = spellsFill.offsetMax;
+
+        spellsFill.offsetMin = new Vector2(0, min.y);
+        spellsFill.offsetMax = new Vector2(-currentSpellsFill, max.y);
     }
 }
