bf8507b455da85f8f4162c66379bba0b7f175a00
Danat Ali Muradov <danatmuradov500@gmail.com>
2025-12-03 00:54:47 -0500

    Fixed the pause menu and set it up for the first level. Options tab is left to do.

--- BurdenOfTheKnight/Assets/Scripts/PauseController.cs
+++ BurdenOfTheKnight/Assets/Scripts/PauseController.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PauseController : MonoBehaviour
 {
     public GameObject pausePanel;
+    public GameObject settingsPanel;
+    public GameObject hudPanel;
     public static bool isPaused = false;
+
+    private CursorLockMode previousLockMode = CursorLockMode.None;
+    private bool previousVisibility = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,21 +31,52 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     public void Pause()
     {
+        // Set the previous lock mode and visibility
+        previousLockMode = Cursor.lockState;
+        previousVisibility = Cursor.visible;
+
+        // Set the pause panel to active and pause the game.
         pausePanel.SetActive(true);
+        hudPanel.SetActive(false);
         Time.timeScale = 0f;
         isPaused = true;
+
+        // Set the cursor states
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void OpenSettings()
+    {
+        pausePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+        pausePanel.SetActive(true);
     }
 
     public void Resume()
     {
+        // Resume the game
         pausePanel.SetActive(false);
+        hudPanel.SetActive(true);
         Time.timeScale = 1f;
         isPaused = false;
+
+        // Put back the cursor to previous state.
+        Cursor.visible = previousVisibility;
+        Cursor.lockState = previousLockMode;
     }
     
     public void MainMenu()
     {
         Time.timeScale = 1f;
+        // Set the mouse locks and set visible
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene(0);
     }
 }
